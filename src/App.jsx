2cbf01b2c2ae82548889188d0e1a8afbd45c3382
vikasmamgai <vickymamgai10@gmail.com>
2022-12-01 16:08:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+import Content from './components/Content'
+
+
+function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Content />
+    </div>
+  )
+}
+
+export default App
